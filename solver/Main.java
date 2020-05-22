9b58290f2a8251e686937c1ac151625949d187cc
aditya <adityagupta0700@gmail.com>
2020-05-22 16:06:24 +0530

    reading complex numbers and perform operations on them

--- solver/Main.java
+++ solver/Main.java
@@ -3,7 +3,10 @@ package solver;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -14,35 +17,33 @@ public class Main {
         Scanner scanner = new Scanner(inFile);
         int numOfVariables = scanner.nextInt();
         int numOfEquations = scanner.nextInt();
-        double[] tmpRowArr = new double[numOfVariables + 1];
+        String tmpStr = scanner.nextLine();
+
         Matrix m = new Matrix(numOfVariables, numOfEquations);
         int cnt = 0;
-
-        for (int i = 0; i < numOfEquations * (numOfVariables + 1); i++) {
-            tmpRowArr[i % (numOfVariables + 1)] = scanner.nextDouble();
-            cnt++;
-            if (cnt == numOfVariables + 1) {
-                cnt = 0;
-                m.addRowInMatrix(tmpRowArr);
-            }
+        String[] tmpRowArr;
+        for (int i = 0; i < numOfEquations; i++) {
+            tmpRowArr = scanner.nextLine().split(" ", 0);
+            System.out.println("Printing row - " + Arrays.toString(tmpRowArr));
+            m.addRowInMatrix(tmpRowArr);
         }
         scanner.close();
 
-        m.solveEquations();
-
-        PrintWriter writer = new PrintWriter(outFile);
-        if (m.ans.equals("No solutions") || m.ans.equals("Infinitely many solutions")) {
-            System.out.println(m.ans);
-            writer.println(m.ans);
-        } else {
-            m.printSolution();
-            System.out.print("The Solution is: (");
-            for (int i = 0; i < numOfVariables; i++) {
-                System.out.print(m.solArr[i] + " ");
-                writer.println(m.solArr[i]);
-            }
-            System.out.print(")");
-        }
-        writer.close();
+//        m.solveEquations();
+//
+//        PrintWriter writer = new PrintWriter(outFile);
+//        if (m.ans.equals("No solutions") || m.ans.equals("Infinitely many solutions")) {
+//            System.out.println(m.ans);
+//            writer.println(m.ans);
+//        } else {
+//            m.printSolution();
+//            System.out.print("The Solution is: (");
+//            for (int i = 0; i < numOfVariables; i++) {
+//                System.out.print(m.solArr[i] + " ");
+//                writer.println(m.solArr[i]);
+//            }
+//            System.out.print(")");
+//        }
+//        writer.close();
     }
 }
